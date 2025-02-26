8b088d76ca33c960e2c807c69c24acf5c86591c7
Ian Clark <icc20@cam.ac.uk>
2025-02-26 09:47:19 +0000

    Fix doubled concat operators

--- src/letswifi/profile/generator/mobileconfiggenerator.php
+++ src/letswifi/profile/generator/mobileconfiggenerator.php
@@ -316,35 +316,35 @@ class MobileConfigGenerator extends AbstractGenerator
                                 . "\n" . '      </dict>';
 
 			} elseif( $network instanceof WiredNetwork ) {
-                                $result .= '<dict>' .
-                                . "\n" .'	<key>EAPClientConfiguration</key>' .
-                                . "\n" .'       <dict>' .
-				. "\n" .'               <key>AcceptEAPTypes</key>' .
-				. "\n" .'               <array>' .
-	         		. "\n" .'                       <integer>13</integer>' .
-	        		. "\n" .'               </array>' .
-				. "\n" .'               <key>TLSTrustedServerNames</key>' .
+                                $result .= '<dict>'
+                                . "\n" .'	<key>EAPClientConfiguration</key>'
+                                . "\n" .'       <dict>'
+				. "\n" .'               <key>AcceptEAPTypes</key>'
+				. "\n" .'               <array>'
+	         		. "\n" .'                       <integer>13</integer>'
+	        		. "\n" .'               </array>'
+				. "\n" .'               <key>TLSTrustedServerNames</key>';
 				        foreach ( $tlsAuthMethod->getServerNames() as $serverName ) {
 					        $result .= '					<string>' . static::e( $serverName ) . '</string>'
 						        . "\n";
 				        }
-			        . "\n" .'       </dict>' .
-			        . "\n" .'       <key>Interface</key>' .
-			        . "\n" .'       <string>AnyEthernet</string>' .
-			        . "\n" .'       <key>PayloadDisplayName</key>' .
-                                . "\n" .'       <string>802.1X Ethernet: Global</string>' .
-                                . "\n" .'       <key>PayloadIdentifier</key>' .
-			        . "\n" .'       <string>' / static::e( $identifier ) . '.wired.' . $payloadNetworkCount . '</string>' .
-                                . "\n" .'       <key>PayloadType</key>' .
-			        . "\n" .'       <string>com.apple.globalethernet.managed</string>' . 
-                                . "\n" .'       <key>PayloadUUID</key>' .
-                                . "\n" .'       <string>FB617606-203D-4B7C-90AB-2DF36BB3FEE9</string>' .
-                                . "\n" .'       <key>PayloadVersion</key>' . 
-                                . "\n" .'       <integer>1</integer>' .
-                                . "\n" .'       <key>SetupModes</key>' .
-                                . "\n" .'       <array>' . 
-                                . "\n" .'               <string>System</string>' .
-                                . "\n" .'       </array>' .
+                                $result .= '       </dict>' .
+			        . "\n" .'       <key>Interface</key>'
+			        . "\n" .'       <string>AnyEthernet</string>'
+			        . "\n" .'       <key>PayloadDisplayName</key>'
+                                . "\n" .'       <string>802.1X Ethernet: Global</string>'
+                                . "\n" .'       <key>PayloadIdentifier</key>'
+			        . "\n" .'       <string>' / static::e( $identifier ) . '.wired.' . $payloadNetworkCount . '</string>'
+                                . "\n" .'       <key>PayloadType</key>'
+			        . "\n" .'       <string>com.apple.globalethernet.managed</string>' 
+                                . "\n" .'       <key>PayloadUUID</key>'
+                                . "\n" .'       <string>FB617606-203D-4B7C-90AB-2DF36BB3FEE9</string>'
+                                . "\n" .'       <key>PayloadVersion</key>' 
+                                . "\n" .'       <integer>1</integer>'
+                                . "\n" .'       <key>SetupModes</key>'
+                                . "\n" .'       <array>' 
+                                . "\n" .'               <string>System</string>'
+                                . "\n" .'       </array>'
                                 . "\n" .'</dict>';
                         } else {
 				throw new InvalidArgumentException( 'Only SSID, Hotspot 2.0, IKEv2 or wired networks are supported, got ' . $network::class );
